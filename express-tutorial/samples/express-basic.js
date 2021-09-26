da9b437aa0ff55d82a77e1fecbb9f08e0e0fe4cb
Joseph Jabbour <jabbour.joseph@gmail.com>
2021-09-26 20:17:55 +0300

    added express samples

--- express-tutorial/samples/express-basic.js
+++ express-tutorial/samples/express-basic.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static('./navbar-app'));
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.status(200).send('Home page');
+});
+app.get('/about', (req, res) => {
+    res.status(200).send('About page');
+});
 app.all('*', (req, res) => {
     res.status(404).send('<h1>resource not found</h1>');
 });
